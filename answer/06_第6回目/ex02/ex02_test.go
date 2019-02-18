6a94c4f8db108544bb43a47016fd2e7241ea2f6c
Kon-y <kon@rosso-tokyo.co.jp>
2019-02-18 23:33:27 +0900

    201902182333 update

--- answer/06_第6回目/ex02/ex02_test.go
+++ answer/06_第6回目/ex02/ex02_test.go
@@ -17,20 +17,20 @@ func TestCalc(t *testing.T) {
 
 	a, b := 1, 2
 
-	Plus_want := 3
-	Minus_want := -1
-	Multi_want := 2
-	Divi_want := float32(0.5)
+	Pluswant := 3
+	Minuswant := -1
+	Multiwant := 2
+	Diviwant := float32(0.5)
 
 	plus, minus, multi, divi := Calc(a, b)
 
-	if plus != Plus_want {
+	if plus != Pluswant {
 		t.Errorf("%d is not Plus ans", plus)
-	} else if minus != Minus_want {
+	} else if minus != Minuswant {
 		t.Errorf("%d is not Minus ans", minus)
-	} else if multi != Multi_want {
+	} else if multi != Multiwant {
 		t.Errorf("%d is not Multi ans", multi)
-	} else if divi != Divi_want {
+	} else if divi != Diviwant {
 		t.Errorf("%f is not Divi ans", divi)
 	}
 }
